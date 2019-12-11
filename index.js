f00a9e2e7494d50931be302a38c7fd822ceb0039
github username <harr.hughes@gmail.com>
2019-12-11 16:46:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+
+    let formData = {name: name, email: email};
+
+    let configObj = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json"}, 
+        body: JSON.stringify(formData)
+    };
+ 
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            document.body.innerHTML = response[ "id" ]
+        })
+        .catch(function(error) {
+            // alert("Bad things! Ragnarők!");
+            document.body.append(error.message);
+        });
+
+}
